7096c5bb847bbeabcccf7017f12275a941cd0a36
Eric Savvy <eric.chen0121@gmail.com>
2013-07-31 00:04:08 -0700

    validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,16 +4,16 @@ class Event < ActiveRecord::Base
 	validates :organizer_name, :title, :date, :organizer_email, presence: true
 	validates :title, uniqueness: true
 	validates :organizer_email, format: {with: /\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\b/}
-	validate :date_is_valid
-	validate :date_is_in_future
+	# validate :date_is_valid
+	# validate :date_is_in_future
 
 
-	def date_is_valid
-		errors.add(:date, "must be a valid date.") if ((DateTime.parse(self.date) rescue ArgumentError) == ArgumentError) 
-	end
+	# def date_is_valid
+	# 	errors.add(:date, "must be a valid date.") if ((DateTime.parse(self.date) rescue ArgumentError) == ArgumentError) 
+	# end
 
-	def date_is_in_future		
-		errors.add(:date, "must be in the future.") if (((DateTime.parse(self.date) < Date.today) rescue ArgumentError) == ArgumentError) 
-	end
+	# def date_is_in_future		
+	# 	errors.add(:date, "must be in the future.") if (((DateTime.parse(self.date) < Date.today) rescue ArgumentError) == ArgumentError) 
+	# end
 
 end
